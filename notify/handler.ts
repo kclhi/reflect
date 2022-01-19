8305b41db2d30ec55f9d7fdeb545cbad8caea4fe
Martin Chapman <contact@martinchapman.co.uk>
2022-01-19 23:15:27 +0000

    Reference generic vendor id

--- notify/handler.ts
+++ notify/handler.ts
@@ -6,7 +6,6 @@ import amqp, { Connection, Channel } from 'amqplib';
 import Withings from './lib/withings';
 
 export default async(event:EventPayload, context:ContextPayload):Promise<ContextPayload> => {
-  
   const logger = winston.createLogger({level:'debug', format:winston.format.simple(), transports:[new winston.transports.Console({stderrLevels:['error']})]});
   const config:Config = require('./config.json');
   logger.debug('received notification: '+JSON.stringify(event.body));
@@ -14,11 +13,15 @@ export default async(event:EventPayload, context:ContextPayload):Promise<Context
   // ~mdc send positive response to withings regardless (also acks initial HEAD request from withings when setting up notifications)
   if(!event.body) { logger.debug('no request body, stopping...'); return context.code(200).send(''); }
 
+  let notification:Notification = event.body as Notification;
+  let userId:string = notification.userid;
+  if(!userId) { logger.debug('no user id, stopping...'); return context.code(200).send(''); }
+  logger.debug('got user id');
+
   // get patient id to associate this data with
   let getPatientId:Response<PatientId>;
   try {
-    let userId:string = (event.body as Notification).userid;
-    getPatientId = await got.post<PatientId>(process.env.internal_service_url+'/id', {json:{"withingsId":userId}, username:process.env.user, password:process.env.password, responseType:'json'});
+    getPatientId = await got.post<PatientId>(process.env.internal_service_url+'/id/withings', {json:{"vendorId":userId}, username:process.env.user, password:process.env.password, responseType:'json'});
     if(getPatientId.statusCode!=200) throw new Error(getPatientId.statusCode+' '+getPatientId.body.toString());
   } catch(error) {
     logger.error('error getting patient id: '+error);
@@ -30,8 +33,7 @@ export default async(event:EventPayload, context:ContextPayload):Promise<Context
   // get access token to get data
   let getToken:Response<Token>;
   try {
-    let userId:string = (event.body as Notification).userid;
-    getToken = await got.post<Token>(process.env.internal_service_url+'/token', {json:{"withingsId":userId}, username:process.env.user, password:process.env.password, responseType:'json'});
+    getToken = await got.post<Token>(process.env.internal_service_url+'/token', {json:{"vendorId":userId}, username:process.env.user, password:process.env.password, responseType:'json'});
     if(getToken.statusCode!=200) throw new Error(getToken.statusCode+' '+getToken.body.toString());
   } catch(error) {
     logger.error('error getting token: '+error);
@@ -41,7 +43,7 @@ export default async(event:EventPayload, context:ContextPayload):Promise<Context
   logger.debug('got token');
 
   // get data
-  let params:any = {action:'getmeas', userid:(event.body as Notification).userid, [config.WITHINGS_API_DATA.START.getmeas]:(event.body as Notification).startdate, [config.WITHINGS_API_DATA.END.getmeas]:(event.body as Notification).enddate};
+  let params:any = {action:'getmeas', userid:notification.userid, [config.WITHINGS_API_DATA.START.getmeas]:notification.startdate, [config.WITHINGS_API_DATA.END.getmeas]:notification.enddate};
   let getData:Response;
   try {
     const dataUrl:string = config.WITHINGS_API_DATA.URLS.getmeas+'?access_token='+getToken.body.token+'&'+Withings.genQueryString(params);
@@ -63,10 +65,10 @@ export default async(event:EventPayload, context:ContextPayload):Promise<Context
   let sbp:number = Withings.getReading(translatedBodyData, 'systolic');
   let dbp:number = Withings.getReading(translatedBodyData, 'diastolic');
   let hr:number = Withings.getReading(translatedBodyData, 'heart');
-
+  logger.debug('received values: '+sbp+' '+dbp+' '+hr);
+  
   // send data
   if(sbp&&dbp&&hr) {
-    logger.debug('received values: '+sbp+' '+dbp+' '+hr);
     let opts:any, connection:Connection;
     try {
       opts = {cert:await fs.readFile(process.env.cert_path), key:await fs.readFile(process.env.key_path), ca:[await fs.readFile(process.env.ca_path)] }
