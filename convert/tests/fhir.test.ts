06c335e9d4c9c6862813093b6c9dcef00e0eeb2a
Martin Chapman <contact@martinchapman.co.uk>
2022-01-23 01:33:06 +0000

    Function tests

--- convert/tests/fhir.test.ts
+++ convert/tests/fhir.test.ts
@@ -2,8 +2,10 @@ import {expect} from 'chai';
 import Fhir from '../lib/fhir';
 
 describe('fhir', () => {
-  it('should be able to generate an observation resource', async() => {
+  it('should be able to generate a patient resource', async() => {
     expect(Fhir.createPatientResource('foo', 'bar')).to.have.property('identifier');
+  }).timeout(0);
+  it('should be able to generate observation resources', async() => {
     expect(Fhir.createBpResource('baz', 200, 60, 60)).to.have.property('subject');
     expect(Fhir.createHrResource('qux', 60, 100, 0.3)).to.have.property('subject');
   }).timeout(0);
